e4e934a538388df48171072409ace3fab2a065e7
ivanzhang1 <zxlingjsj@163.com>
2020-04-25 14:05:07 +0800

    test

--- ServiceTesting/src/main/java/com/rainivan/formula/FunctionString.java
+++ ServiceTesting/src/main/java/com/rainivan/formula/FunctionString.java
@@ -5,4 +5,9 @@ public class FunctionString {
 	public void strReverse() {
 		System.out.println("First to show the string reverse.");
 	}
+	
+	public static void main(String[] args) {
+		FunctionString fstr = new FunctionString();
+		fstr.strReverse();
+	}
 }
