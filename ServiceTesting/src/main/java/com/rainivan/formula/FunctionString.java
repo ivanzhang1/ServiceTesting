58942cfabccd8cdd2b7993b84e5a0b401577f85b
ivanzhang1 <zxlingjsj@163.com>
2020-04-26 10:48:28 +0800

    add function

--- ServiceTesting/src/main/java/com/rainivan/formula/FunctionString.java
+++ ServiceTesting/src/main/java/com/rainivan/formula/FunctionString.java
@@ -2,13 +2,16 @@ package com.rainivan.formula;
 
 public class FunctionString {
 	
-	public void strReverse() {}
+	public void strReverse() {
+		System.out.println("First to show the string reverse.");
+	}
 	
 	public static void main(String[] args){
-//		FunctionString fstr = new FunctionString();
-//		fstr.strReverse();
+		FunctionString fstr = new FunctionString();
+		fstr.strReverse();
 		
-		System.out.println("First to show the string reverse.");
+		
+		
 		
 	}
 }
